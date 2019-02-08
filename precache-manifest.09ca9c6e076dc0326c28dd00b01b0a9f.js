7acc64d2b2eef8341980e4267a5e23e37992e9ed
[trapintrovert] <[mcneo062@gmail.com]>
2019-02-08 04:20:32 +0100

    Updates

---
+++ precache-manifest.09ca9c6e076dc0326c28dd00b01b0a9f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e89ff6257d8eb31409c0d2a59aee9ab",
+    "url": "/medkit/static/media/why-bg_2.2e89ff62.svg"
+  },
+  {
+    "revision": "edf20aab50d1691e8b88",
+    "url": "/medkit/static/js/main.edf20aab.chunk.js"
+  },
+  {
+    "revision": "dcfa6cdc6bc87d5b595e636df8e2c939",
+    "url": "/medkit/static/media/avatar03.dcfa6cdc.svg"
+  },
+  {
+    "revision": "44a02b42c47ed34eadfdb2cb3f64a805",
+    "url": "/medkit/static/media/avatar02.44a02b42.svg"
+  },
+  {
+    "revision": "d80877a658b147fd4c3247b3f72c7b0c",
+    "url": "/medkit/static/media/avatar01.d80877a6.svg"
+  },
+  {
+    "revision": "ff58ebfefce76c7e53917ed1bf85ae19",
+    "url": "/medkit/static/media/image06.ff58ebfe.svg"
+  },
+  {
+    "revision": "d1f980731f17e61cf467",
+    "url": "/medkit/static/js/runtime~main.d1f98073.js"
+  },
+  {
+    "revision": "51dba2fceeb1fbcf2640c6f6c86e84b8",
+    "url": "/medkit/static/media/hero-bg.51dba2fc.svg"
+  },
+  {
+    "revision": "79f2cd6d99d0f23f86c6d87acdd0d2bf",
+    "url": "/medkit/static/media/facebook.79f2cd6d.svg"
+  },
+  {
+    "revision": "ad204e505fd3dd4a5d04a2ef41ac51d0",
+    "url": "/medkit/static/media/Twitter.ad204e50.svg"
+  },
+  {
+    "revision": "4dec6ac397e808bbe5566a83821c519c",
+    "url": "/medkit/static/media/whatsapp.4dec6ac3.svg"
+  },
+  {
+    "revision": "95ae6e8d2fb536db1024a4e91e17b7ab",
+    "url": "/medkit/static/media/instagram.95ae6e8d.svg"
+  },
+  {
+    "revision": "918dfbb4f827603ae4e45e34179e756d",
+    "url": "/medkit/static/media/image05.918dfbb4.svg"
+  },
+  {
+    "revision": "679f01af84962bb94a546a976310fa1d",
+    "url": "/medkit/static/media/doctors.679f01af.svg"
+  },
+  {
+    "revision": "26a9e2048f735d8ddd62f633bd894bf6",
+    "url": "/medkit/static/media/image01.26a9e204.svg"
+  },
+  {
+    "revision": "a029cbf044c2c5ba34243dd5465a6c1c",
+    "url": "/medkit/static/media/image02.a029cbf0.svg"
+  },
+  {
+    "revision": "08ea4297aac189bb91e7c5c26a62f562",
+    "url": "/medkit/static/media/image03.08ea4297.svg"
+  },
+  {
+    "revision": "5ae2c5872e07fcb395f3f771bcb2571e",
+    "url": "/medkit/static/media/image04.5ae2c587.svg"
+  },
+  {
+    "revision": "3881845e78d55e2d7c5e",
+    "url": "/medkit/static/js/3.3881845e.chunk.js"
+  },
+  {
+    "revision": "737a11b873e6e6d140d0",
+    "url": "/medkit/static/js/2.737a11b8.chunk.js"
+  },
+  {
+    "revision": "06f19914aa3bef3e36d0",
+    "url": "/medkit/static/js/1.06f19914.chunk.js"
+  },
+  {
+    "revision": "737a11b873e6e6d140d0",
+    "url": "/medkit/static/css/2.52d96341.chunk.css"
+  },
+  {
+    "revision": "06f19914aa3bef3e36d0",
+    "url": "/medkit/static/css/1.4b424739.chunk.css"
+  },
+  {
+    "revision": "e6d56866de8df334c100234de5924cae",
+    "url": "/medkit/index.html"
+  }
+];
